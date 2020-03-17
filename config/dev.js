4e2a2c1aa739853940bfbfaabbc015da3d71a9e9
dorian <lauren_d@outlook.fr>
2020-03-17 16:44:15 +0100

    push

--- config/dev.js
+++ config/dev.js
@@ -4,6 +4,7 @@ const common = require('./common.js');
 module.exports = merge(common, {
 	mode: 'development',
 	devtool: 'inline-source-map',
+	disableHostCheck: true,
 	devServer: {
 		contentBase: './dist',
 		host: '0.0.0.0'
