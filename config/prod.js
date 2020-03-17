3f9b1a026b2add8b7d7e5881a6760a557f6550d0
dorian <lauren_d@outlook.fr>
2020-03-17 16:37:30 +0100

    push

--- config/prod.js
+++ config/prod.js
@@ -2,5 +2,10 @@ const merge = require('webpack-merge');
 const common = require('./common.js');
 
 module.exports = merge(common, {
-	mode: 'production'
+	mode: 'production',
+	devServer: {
+	    disableHostCheck: true,
+	    host: '0.0.0.0',
+	    port: 8080
+	}
 });
