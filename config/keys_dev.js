75254ad0f814c48c43e7dd00b3b1bb6b128bc6a2
dorian <lauren_d@outlook.fr>
2020-03-18 16:10:01 +0100

    push

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stripePublishableKey:'pk_live_beyKZnTzonNc2Qam3Fv9XLfv00T88Th6Jo',
+  stripeSecretKey:'sk_live_bhVd7WVQcHoobTQwAVPL45aO00t3p9BdN1'
+}
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const express = require('express');
+const keys = require('./config/keys_dev');
+const stripe = require('stripe')(keys.stripeSecretKey);
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 const webpack = require('webpack');
 const webpackDevMiddleware = require('webpack-dev-middleware');
 
@@ -6,12 +10,36 @@ const app = express();
 const config = require('./config/common.js');
 const compiler = webpack(config);
 
-// Tell express to use the webpack-dev-middleware and use the webpack.config.js
-// configuration file as a base.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
 }));
 
-// Serve the files on port 3000.
-app.listen(8081, function () {
+app.get('/', (req, res) => {
+  res.render('index', {
+    stripePublishableKey: keys.stripePublishableKey
+  });
+});
+
+app.post('/charge', (req, res) => {
+  const amount = 3;
+  
+  stripe.customers.create({
+    email: req.body.stripeEmail,
+    source: req.body.stripeToken
+  })
+  .then(customer => stripe.charges.create({
+    amount,
+    description: 'Cours de tamoul',
+    currency: 'eur',
+    customer: customer.id
+  }))
+  .then(charge => res.render('success'));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, function () {
 });
