400509adeb305e71e67fd371254e40624ae5b1f7
dorian <lauren_d@outlook.fr>
2020-03-23 17:21:44 +0100

    to

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const express = require('express');
 const webpack = require('webpack');
 const keys = require('./config/keys_dev.js');
 const bodyParser = require('body-parser');
-const nodemailer = require("nodemailer");
 const webpackMiddleware = require('webpack-dev-middleware');
 const webpackHotMiddleware = require('webpack-hot-middleware');
 const config = require('./config/common.js');
+const nodemailer = require('nodemailer');
 const stripe = require('stripe')(keys.stripeSecretKey);
 
 const isDeveloping = process.env.NODE_ENV !== 'production';
@@ -37,7 +37,7 @@ if (isDeveloping) {
 		res.sendFile(__dirname +"index.html");
 	});
 	app.get('/code',(req,res)=>{
-	    res.json({code: keys.stripePublishableKey});
+		res.json({code: keys.stripePublishableKey});
 	});
 	app.post('/charge', (req, res) => {
 		const amount = 300;
@@ -51,6 +51,52 @@ if (isDeveloping) {
 			currency: 'eur',
 			customer: customer.id
 		}))
+		.then(() => {
+			var mailOpts, smtpTrans;
+
+			smtpTrans = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user: "sopika.lkdigital@gmail.com",
+					pass: "Laurenceau09"
+				}
+			});
+			var mailoutput = "<html>\n\
+			<body>\n\
+			<table>\n\
+			<tr>\n\
+			<td>Bonjour !</td>\n\
+			</tr><br>\n\
+			<tr>\n\
+			<td>Tout d'abord, merci pour l'interêt que vous portez à mes cours.<br>Voici le cours en pièce jointe ! N'hésitez pas à me faire un retour, j'espère qu'il vous aidera !</td>\n\
+			</tr><br>\n\
+			<tr>\n\
+			<td>Tout PS: Tout achat n'est pas remboursable</td>\n\
+			</tr><br>\n\
+			<tr>\n\
+			<td>Sopika Uthayakumar<br>\n\
+			<a href='https://uthaya.fr'>Uthaya</a>\n\
+			</td>\n\
+			</tr>\n\
+			</table></body></html>";
+
+			mailOpts = {
+				to: req.body.stripeEmail,
+				attachments: [{
+					filename: req.body.cours + '.pdf',
+					path: path.join(__dirname, '/static/files/' + req.body.cours + '.pdf'),
+					contentType: 'application/pdf'
+				}],
+				subject: "Uthaya - Merci pour l'achat - Voici votre cours !",
+				html: mailoutput
+			};
+
+			smtpTrans.sendMail(mailOpts, function (error, res) {
+				if (error) {
+					return console.log(error);
+				}
+			});
+		})
 		.then(charge => res.redirect('/'));
 	});
 } else {
@@ -74,7 +120,7 @@ if (isDeveloping) {
 		res.sendFile(__dirname +"index.html");
 	});
 	app.get('/code',(req,res)=>{
-	    res.json({code: keys.stripePublishableKey});
+		res.json({code: keys.stripePublishableKey});
 	});
 	app.post('/charge', (req, res) => {
 		const amount = 300;
@@ -88,6 +134,52 @@ if (isDeveloping) {
 			currency: 'eur',
 			customer: customer.id
 		}))
+		.then(() => {
+			var mailOpts, smtpTrans;
+
+			smtpTrans = nodemailer.createTransport({
+				service: 'gmail',
+				auth: {
+					user: "sopika.lkdigital@gmail.com",
+					pass: "Laurenceau09"
+				}
+			});
+			var mailoutput = "<html>\n\
+			<body>\n\
+			<table>\n\
+			<tr>\n\
+			<td>Bonjour !</td>\n\
+			</tr><br>\n\
+			<tr>\n\
+			<td>Tout d'abord, merci pour l'interêt que vous portez à mes cours.<br>Voici le cours en pièce jointe ! N'hésitez pas à me faire un retour, j'espère qu'il vous aidera !</td>\n\
+			</tr><br>\n\
+			<tr>\n\
+			<td>Tout PS: Tout achat n'est pas remboursable</td>\n\
+			</tr><br>\n\
+			<tr>\n\
+			<td>Sopika Uthayakumar<br>\n\
+			<a href='https://uthaya.fr'>Uthaya</a>\n\
+			</td>\n\
+			</tr>\n\
+			</table></body></html>";
+
+			mailOpts = {
+				to: req.body.stripeEmail,
+				attachments: [{
+					filename: req.body.cours + '.pdf',
+					path: path.join(__dirname, '/static/files/' + req.body.cours + '.pdf'),
+					contentType: 'application/pdf'
+				}],
+				subject: "Uthaya - Merci pour l'achat - Voici votre cours !",
+				html: mailoutput
+			};
+
+			smtpTrans.sendMail(mailOpts, function (error, res) {
+				if (error) {
+					return console.log(error);
+				}
+			});
+		})
 		.then(charge => res.redirect('/'));
 	});
 }
