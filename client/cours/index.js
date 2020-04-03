58beafb3b3a8a8ed0e98b564bdf3cd8b036c2a4c
dorian <lauren_d@outlook.fr>
2020-04-03 20:46:20 +0200

    toto

--- client/cours/index.js
+++ client/cours/index.js
@@ -1,4 +1,27 @@
 require("./index.scss");
 require("../common/common.js");
+import $ from 'jquery';
+import 'jquery-scrollify';
 
-console.log("client/page2/index.js");
+$(function () {
+	$.scrollify({
+		section: ".scrolls",
+		sectionName : "section-name",
+		scrollSpeed: 800,
+		scrollbars: true,
+		before: function () {
+			$('.scrolls').removeClass('active')
+			var current = $.scrollify.current();
+			current.addClass('active')
+		}
+
+
+	});
+
+})
+
+$(".menu__item-link").click(function(e) {
+	e.preventDefault();
+	console.log($(this).attr("href"))
+	$.scrollify.move($(this).attr("href"));
+});
