a39585bce222ddde076053fe5d35e8240853a38d
dorian <lauren_d@outlook.fr>
2020-04-10 15:35:13 +0200

    toto

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,4 +5,5 @@ module.exports = function (app) {
     app.use('/ateliers', require('./ateliers'));
     app.use('/presentation', require('./presentation'));
     app.use('/boutique', require('./boutique'));
+    app.use('/*', require('./home'));
 };
